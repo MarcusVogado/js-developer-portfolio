c7312d5c916162938a5fcdd7ddfc95d7caaaf428
MarcusVogado <marcus_620@hotmail.com>
2023-07-31 21:40:13 -0300

    Conclusão

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.com/MarcusVogado/js-developer-portfolio/blob/main/data/profile.json';
+    const url = 'https://formacao-dio.marcusvogado.com/trilha-js/portfolio/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
